f0330d6c64c37e985431e06f1665dff9faef3b1f
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-25 14:10:37 +1000

    fixed syntax errors

--- Intro to python/test.py
+++ Intro to python/test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 example= "this is a python example script to check git commits"
 
-example2: "checking if git has updated new code in script"
+example2= "checking if git has updated new code in script"
 
 example3="I want to see how can i see tracked changes in code "
 
