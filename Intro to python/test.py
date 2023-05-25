48028da18228dc30989f92c3935a5abfcde4db46
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-25 13:57:14 +1000

    checking if git is gettting updated

--- Intro to python/test.py
+++ Intro to python/test.py
@@ -1,5 +1,11 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
 example= "this is a python example script to check git commits"
 
+example2: "checking if git has updated new code in script"
+
 print(example)
+print(example2)
 
 #adding some new code in the script 
