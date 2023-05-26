50cea13f2100f3184452c823ada464fe3ec8d2c9
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-26 15:43:54 +1000

    formatting strings usings placeholders

---
+++ Learning Python/Print_Formatting.py
@@ -0,0 +1,21 @@
+#formatting strings can be done in various ways!
+
+#through concatenation, fstrings, placeholders, pr=int formtting 
+
+
+print("python  programming is growing  %s here" %'popular') #can use placeholders for formatting print statement
+
+print("Python is utilized in many ways to interpret %s" %'data')
+
+
+print("Using %s is a way of string %s" %('placeholders', 'formatting ')) #can place multiple elements through placeholders
+
+
+a , b= 'placeholders' , 'formatting'
+
+print("using %s can be used for string %s" %(a,b)) #variable names can be assigned for string formatting 
+
+
+
+
+
