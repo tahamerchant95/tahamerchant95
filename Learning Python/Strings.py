064a974db5a86eef6886655fd3865a45a0802287
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-26 13:18:19 +1000

    Performing some basic string indexing and slicing

---
+++ Learning Python/Strings.py
@@ -0,0 +1,38 @@
+print ("Have started learning python") #using some print statements from line 1 to 9 
+
+print("Python is interesting")
+
+print("what makes python \n interesting ? ")
+
+print ("\n") # this line specified that we are adding a new line 
+
+print("Because of its easy to understand syntax")
+
+a= len("python") #displays the length of string or the number of characters used in the string 
+
+print(a)
+
+print ("we will show a concept of string indexing")
+
+b= "Python"
+
+#using string indexing, which means can grab any element from the string 
+
+a=b[1] #trying to grab first element of the string python 
+
+c= b[1:] # can slice the index, in this function I'm trying to grab everything after the first index 
+
+d= b[:4] # I'm trying to get anything before the 4th index 
+
+e =b[:] #get everythhig in the string 
+
+f= b [-1] # get the last element 
+
+g= b[::2] # can grab elements in frequency, for instance i have set the indexing by getting two elements at a time 
+
+print(a)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
