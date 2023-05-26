04c46e55786a075e69ce8e2dc870b75ba2e2ebd8
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-26 12:52:55 +1000

    Some basic arithemtic functions plus variable init

---
+++ Learning Python/Numbers.py
@@ -0,0 +1,23 @@
+a= 2 + 1 #addition
+print (a)
+
+b= 3 - 1 #subtraction
+print(b)
+
+c= 3 * 2 #multiplication
+print(c)
+
+d= 3 / 2 #division
+print(d)
+
+e= 7 // 2 #floor division which truncates decimal and gives round off figure without giving remainder 
+print(e)
+
+f= 7%2 #if we only want to see remainder 
+print(f)
+
+# have assigned variables to perform some basic arithimetic functions
+
+
+
+
