2d1ac617991fe7ccc0c483dc247bfcf446a5cabe
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-26 13:35:03 +1000

    added some additional string functions

--- Learning Python/Strings.py
+++ Learning Python/Strings.py
@@ -20,7 +20,7 @@ b= "Python"
 
 a=b[1] #trying to grab first element of the string python 
 
-c= b[1:] # can slice the index, in this function I'm trying to grab everything after the first index 
+c= b[1:] # can slice the index, in this function I'm trying to grab everything from first index to last 
 
 d= b[:4] # I'm trying to get anything before the 4th index 
 
@@ -30,9 +30,34 @@ f= b [-1] # get the last element
 
 g= b[::2] # can grab elements in frequency, for instance i have set the indexing by getting two elements at a time 
 
+
+h= "Is an interesting language "
+i= "one of the most popular languages in todays time"
+
+k= h + i
+
+#python comes with alot of arthimetic functions to make changes to strings, numbers and other data elements
+
+j=k.upper() #using upper functions allows us to capitaliza whole string 
+l= k.lower() #using lower functions allows us to not capitalize the whole string 
+
+m= k.split() #using split function allows to split tge string with blank spaces 
+
+print(m)
+
+print(l)
+print(j)
+
+print(k) #concatenating strings, meaning can add two strings together to create a single statement 
 print(a)
 print(c)
 print(d)
 print(e)
 print(f)
 print(g)
+print(j)
+
+
+print("Insert another string: {} ".format("inserted string ") ) #print format statements 
+
+
