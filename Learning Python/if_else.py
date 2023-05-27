04a302edfb3cb442cc5f5d84b4e739a470b27cc9
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-28 00:51:14 +1000

    added for loops using dictionaries, sets, and tups

--- Learning Python/if_else.py
+++ Learning Python/if_else.py
@@ -19,3 +19,80 @@ elif fruit =='Apple': #if condition two true execute this statement
 else:
     print("No fruits are available to buy") #final condition
 
+
+
+# for loops help to iterate over items
+# can iterate over strings, tuples, lists, keys, and values 
+
+
+lst1= [1,2,3,4,5,6,7,8,9] #created a list 
+
+for numbers in lst1: #iteration using for loop
+    print(numbers)
+
+
+print (18%5) #using modulo allows us to get remainder value, 18 divided by 5 gives a remainder of 3
+
+
+#we will try to find even numbers in our list that we have created
+#we can know even numbers when a number is fully divisible giving a remainder of zero 
+
+for numbers in lst1: #this allows us to use a for loop to iterate over list and specifiy a condition using if
+    if numbers % 2 == 0 :
+        print(numbers)
+    else:
+        print("Number is odd")
+
+
+lst_sum= 0 
+
+for numbers in lst1:
+    #lst_sum= lst_sum + numbers #performing addition of all elements in the list 
+
+    lst_sum += numbers #another way of doing this using comparison operators
+
+print(lst_sum)
+
+
+for items in 'Python': #using strings with for loop to iterate in sequence 
+    print(items)
+
+
+sample= (5,4,3,2,1)
+
+for items in sample:
+    print(items)
+
+# when using for loops with tuple, list containing tuples can be unpacked as well
+
+sample1= [(1,2), (3,4), (5,6)] #creating a list contaning tuples 
+
+for items in sample1: #first we try to print items in list
+    print(items)
+
+for (t1,t2) in sample1: #unpacking tuples 
+    print(t2)
+
+# for loops can also be used with dictionary values 
+
+dict_1={'k1': 1, 'k2': 2, 'k3': 3}
+
+print (dict_1.keys()) #print keys
+
+print(dict_1.values()) # print values
+
+print(dict_1.items()) # print items which also support iteration 
+
+for k,v in dict_1: # using for loop to print keys and values 
+    print(k)
+    print(v)
+
+
+print(list(dict_1.values())) #casting list to get values
+
+print(list(dict_1.keys())) #casting list to get keys 
+
+# dictionaries are unorderedd so will need to use sort function 
+
+
+
