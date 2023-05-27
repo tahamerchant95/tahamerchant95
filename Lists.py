55e32d1808691ae927b401f2551067399f96f87b
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-27 12:20:06 +1000

    More listing examples added

--- Lists.py
+++ Lists.py
@@ -52,6 +52,44 @@ print (example_list)
 # if want to remove specific index will need to specify the actual index 
 
 
-example_list.pop(7) #want to remove the second item from list 
+example_list.pop(7) #want to remove the seventh item from list 
 
 print(example_list)
+
+
+#1. Reversing list
+
+example_list.reverse() #when reversing, the change is going to be permanent 
+print(example_list)
+
+#2. sorting list 
+#example_list.sort() #if you run this part, it will generate a TypeError since lists cannot contain float values if this function is called
+
+
+example_list2= ['a', 'c', 'b', 'd', 'f', 'e']
+example_list2.sort() #sorting list will give values in alphabetical order
+print(example_list2)
+
+
+#lists can be nested 
+
+list1= [1,2,3] #creating three lists and reassigning them to example_list3
+list2=[4,5,6]
+list3=[7,8,9]
+
+example_list3= [list1,list2, list3]
+print(example_list3)
+
+print(example_list3[:-1]) #indexing in multiple data structures 
+print(example_list3[1:])
+print(example_list3[2:])
+
+
+print(example_list3[0])
+
+print(example_list3[0][0])  # this prints the first index of the list and then the first element from the first list 
+
+
+
+example_list3=[row[0] for row in example_list3] #example of list comprehension 
+print(example_list3)
