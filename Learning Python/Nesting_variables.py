2dfb4e9d1211957bb0111b58f52e0aaafa704ed7
tahamerchant95 <tahamerchant1995@gmail.com>
2023-05-30 10:57:36 +1000

    adding *args and *kwargs examples

--- Learning Python/Nesting_variables.py
+++ Learning Python/Nesting_variables.py
@@ -65,8 +65,67 @@ def glob_func():
     
 glob_func()
 print("Value of y after running func is:" , y)
-print(globals()) # can check global values
-print(locals()) # can check local values 
+#print(globals()) # can check global values
+#print(locals()) # can check local values 
 
 
+# **kwargs and *args will be used at many places in python
+# why ?
+# we will write a simple function to explain this 
 
+
+def my_func(a,b):
+    return sum((a,b))
+
+result=my_func(10,20)* .10 #returning 10 percent of the sum 
+print(result)
+
+# however we can pass multiple params in our function with assigned values
+
+def my_func2(a=0, b=0, c= 0 ): #not an efficient way of producing results 
+    return sum((a,b,c)) * 0.10
+
+result=my_func2(10,20,30)
+print(result)
+
+
+# we will write the above code in efficient way using *args
+# using args takes an arbitary number of arguements 
+# takes them in values as tuples
+# arbitary meaninig any random name 
+# so if want to use any name that takes random values need to use * before word 
+
+def my_func3(*args):
+    return sum((args)) * 0.10 
+
+result=my_func3(10,20) 
+print(result) 
+
+
+def my_func4(*nums):
+    return sum((nums))*0.20
+
+result=my_func4(10,20)
+print(result)
+
+# instead of creating tuple values we can use **kwargs
+# kwargs allow to create dictionary values, meaning keys and values 
+
+def my_func5(**kwargs):
+    if 'product' in kwargs:
+        print(f"My favourite product is {kwargs['product']}")
+    else:
+        print("Dont print anything")
+
+result= my_func5(product='Mangoes')
+print(result)
+    
+def my_func6(*args, **kwargs):
+    if 'fruit' and 'shake' in kwargs:
+        print(f"I like {' and '.join(args)} and my favorite fruit is {kwargs['fruit']}")
+        print(f"May i have some {kwargs['shake']}, shake?")
+    else:
+        pass
+
+result=my_func6('bread', 'omlette', fruit='Mangoes', shake= 'Juice')
+print(result)
