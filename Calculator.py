e35da649318b4183dbc912077f4960ec5bd2958b
tahamerchant95 <tahamerchant1995@gmail.com>
2023-06-10 17:42:26 +1000

    Creating Calculator project

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+# This project is about creating a simple calculator 
+
+# need to ask user to enter two numbers
+
+# add numbers
+# subtract numbers
+# divide numbers 
+# multiple numbers 
+
+# Need to take user input 
+# when user is done with operation, ask user again if want to continue calculating 
+# what if user enters negative numbers? what to do then?
+# what if user enters wrong input 
+# user might enter decimal values 
+# with dividing numbers can throw an error of zero division
+# have to provide user with choices as to what operation needs to carried out 
+# user might enter a string value instead of integers or floats 
+
+
+# first lets print out our choices to ask user 
+
+print("Select choice:")
+print("1. Add:" + "+")
+print("2. Subtract:" + "-")
+print("3. Multiply:" + "*")
+print("4. Divide:" + "/")
+
+
+# lets ask the user to enter choice 
+
