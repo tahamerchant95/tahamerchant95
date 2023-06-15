7408ed95c4545d93025a15b7af8295a03146419a
tahamerchant95 <tahamerchant1995@gmail.com>
2023-06-16 02:16:24 +1000

    creating templates

--- Toys4Us/Shop.py
+++ Toys4Us/Shop.py
@@ -1,14 +1,15 @@
 # Will use Flask to create our website 
+# python uses templating engine called jinja 
 
 from flask import Flask 
 
-from flask import redirect, url_for
+from flask import redirect, url_for, render_template
 
 app= Flask(__name__)
 
 @app.route("/") # created a route to the page
 def new_page():
-    return "This is the new page <h1>hello World</h1>"
+    return render_template("index.html") # rendering template 
 
 @app.route("/<name>") # example of dynamic URL 
 def user(name):
@@ -16,7 +17,7 @@ def user(name):
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home")) # creating redirect function to direct user to different pages 
+    return redirect(url_for("home", name="Admin")) # creating redirect function to direct user to different pages 
 
 if __name__ == "__main__":
     app.run()
