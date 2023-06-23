d333813ad22489afbb569c10454481e7d1f147cb
tahamerchant95 <tahamerchant1995@gmail.com>
2023-06-24 02:37:48 +1000

    making changes in game

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -6,37 +6,43 @@
 # user will get a set number of chances to guess letters 
 # when user guesses all correct letters user wins 
 # if user looses all chances game ends and secret word is printed out 
-
+# what if user enters the same letter again 
 
 secret_word=('python') #we have set the secret word for user to guess
 
 allowed_turns= 5 # user gets 5 chances to guess word
 
-guesses='' # have assigned an empty variable which will be appended whe user guesses any letter
+guesses=[] # have assigned an empty variable which will be appended whe user guesses any letter
 
-while allowed_turns > 0 :
+already_guessed=[]
+
+while allowed_turns > 0 : # will run when allowed turns are greater 
     failed = 0 
 
-    for letters in secret_word:
-        if letters in guesses:
-            print(letters, end="")
+    for letters in secret_word: # will check for characters in our provided word 
+        if letters in guesses: # will check if letters exists in guesses
+            print(letters, end="")      
+        elif letters in already_guessed:
+            print("Youve already guessed that letters, enter another")
         else:
             print("_", end="")
-            failed= failed + 1 
+            failed= failed + 1
 
     if failed == 0:
         print("\nYouve guessed the correct word")
         break
 
     guess=input("\nGuess a character:")
-    guesses = guesses + guess
+    guesses.append(guess)
+    already_guessed.append(guesses)
+
+    for letters in already_guessed:
+        if letters in already_guessed == guesses:
+            print("\nyouve already guessed this letter:")
 
     if guess not in secret_word:
-        allowed_turns= allowed_turns - 1 
-        print("\nWrong guess" + "turns remaining", allowed_turns)
+        allowed_turns= allowed_turns - 1 # decrease the number of turns 
+        print("\nWrong guess! "  +  " turns remaining:", allowed_turns)
         if allowed_turns == 0 :
             print("\nyou lose")
-
-
-
 
---
+++ Hangman/Hangman_pt2.py
@@ -0,0 +1,50 @@
+secret_word = ("python")
+
+turns = 5
+
+wrong_guesses= 0 
+
+used_letters= []
+
+dash_letter= "_" * len(secret_word)
+
+while turns > 0 and dash_letter != secret_word:
+    print("\nfollowing letters have been used:", used_letters)
+    print("\nletters guessed currently:\t", dash_letter)
+    guess= input("Enter guess letter:\t")
+    guess= guess.lower()
+
+    while guess in used_letters:
+        print("Youve already used this letter:\t", guess)
+        guess= input("Guess again:\t")
+        guess=guess.lower()
+    
+    used_letters.append(guess)
+
+    if guess in secret_word:
+        print("Correct guess:",secret_word)
+
+        new =""
+
+        for i in range(len(secret_word)):
+            if guess == secret_word[i]:
+                new = new + guess
+            else:
+                new = new + dash_letter[i]
+
+        dash_letter = new
+    
+    else:
+        print("guess isnt in the word")
+
+    if guess not in secret_word:
+        print("\nWrong guess entered:")
+        turns = turns - 1 
+        print("\nTurns remaining:", turns)
+
+        if turns == 0:
+            print("Youve ran out of turns, \nthe secret word is:", secret_word)
+        else:
+            print("\n youve guessed the correct word:", secret_word)
+
+
