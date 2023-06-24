21859d827ba7942787001b1c6010b5f1ac01fe35
tahamerchant95 <tahamerchant1995@gmail.com>
2023-06-24 17:10:07 +1000

    adding comments for better understanding code

--- Hangman/Hangman_pt2.py
+++ Hangman/Hangman_pt2.py
@@ -1,12 +1,14 @@
-secret_word = ("python")
+secret_word = ("python") # stored a word in our variable, this remains constant
 
-turns = 5
+turns = 5 # maximum number of turns allowed 
 
-wrong_guesses= 0 
+wrong_guesses= 0 # currently wrong guesses are zero 
 
-used_letters= []
+used_letters= [] # initialized an empty list 
 
-dash_letter= "_" * len(secret_word)
+dash_letter= "_" * len(secret_word) # this will print dashes with the number of letters in our word
+
+# in this case we have 6 letters so will print 6 dashes
 
 while turns > 0 and dash_letter != secret_word:
     print("\nfollowing letters have been used:", used_letters)
@@ -26,11 +28,11 @@ while turns > 0 and dash_letter != secret_word:
         print("Correct guess:",secret_word)
         new =""
 
-        for i in range(len(secret_word)):
-            if guess == secret_word[i]:
+        for letters in range(len(secret_word)):
+            if guess == secret_word[letters]:
                 new = new + guess
             else:
-                new = new + dash_letter[i]
+                new = new + dash_letter[letters]
 
         dash_letter = new
 
